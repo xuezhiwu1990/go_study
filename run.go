8eec9b5a9a783377c46e51e40bd20715775eb3dd
xuezhiwu1990 <15117995393@126.com>
2018-06-04 00:09:44 +0800

    Go随机生成访问nginx日记统计脚本

--- run.go
+++ run.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-       "fmt"
-       "strings"
-       "strconv"
+        "fmt"
+        "strings"
+        "strconv"
+		"flag"
+		"net/url"
+		"math/rand"
+		"time"
+		"os"
 )
 
 type resource struct {
@@ -54,19 +59,34 @@ func buildUrl( res []resource) []string {
   return list
 }
 
-//func makelog( current , refer , ua  string) string {
-//  u : = url.Values{}
-//  u.Set("time","1")
-//  u.Set("url",current)
-//  u.Set("refer",refer)
-//  u.Set("ua",ua)
-//  //paramsStr := utf16.Encode()
-//}
+func makelog( current , refer , ua  string) string {
+ u := url.Values{}
+ u.Set("time","1")
+ u.Set("url",current)
+ u.Set("refer",refer)
+ u.Set("ua",ua)
+ paramsStr := u.Encode()
+
+ logTemplate := "192.168.47.1 - - [03/Jun/2018:12:21:01 +0800] 'OPTIONS /dig?{$paramsStr}' '{$ua}' '-'";
+ //logTemplate := "{$paramsStr}{$ua}";
+ log := strings.Replace(logTemplate , "{$paramsStr}",paramsStr,-1)
+ log = strings.Replace(log , "{$ua}",ua,-1)
+ return log
+}
+
+func randInt(min ,max int) int {
+	fmt.Println(min,max)
+	r := rand.New( rand.NewSource(time.Now().UnixNano() ))
+	if min > max {
+		return max
+	}
+	return r.Intn(max-min) + min
+}
 
 func main () {
-  //total := flag.Int( "total" , 100 ,"how many")
-  //filePath := flag.String("filePath","./dig_go.log")
-  //flag.Parse()
+  total := flag.Int( "total" , 100 ,"how many")
+  filePath := flag.String("filePath","D:\\Go_WorkSpace\\dig_go.log" ,"D:\\Go_WorkSpace\\dig_go.log")
+  flag.Parse()
 
   //需要构造出真实的url集合
   res := ruleResource()
@@ -74,8 +94,15 @@ func main () {
   fmt.Println(list)
 
   //按照要求,生成$total 行日志内容
-  //for i:= 0;i<=*total;i++ {
-  //  logStr := makelog(currentUrl , referUrl ,ua )
-  //}
-
+	logStr := ""
+  for i:= 0;i<=*total;i++ {
+	  currentUrl := list[ randInt(0, len(list)-i) ]
+	  referUrl := list[ randInt(0, len(list)-i) ]
+	  ua := "Mozilla/5.0 (Windows NT 10.0.10586; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2950.5 Safari/537.36"
+      logStr = logStr + makelog(currentUrl,referUrl,ua ) + "\n"
+  }
+  fd,_ := os.OpenFile( *filePath , os.O_RDWR | os.O_APPEND , 0644)
+  fd.Write([]byte(logStr))
+  fd.Close()
+  fmt.Println("done.\n")
 }
